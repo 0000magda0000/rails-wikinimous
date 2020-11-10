562a19c42710c6dc7ff6fb7d1aaabc213fb67471
Magda Barthofer <magda.barthofer@gmail.com>
2020-11-10 15:56:51 +0100

    added some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :articles
-  # get 'index', to: 'articles#index', as: :articles
-  # get 'show', to: 'articles#show', as: :article
 end
 
-# , except: [:index, :show]
